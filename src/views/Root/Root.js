e51ebb60c267f0631ed4bed94f9be56eeaca627e
Krzysztof <krzysztof.podmokly@gmail.com>
2019-10-09 22:19:35 +0200

    Adding styled-components and storybook

---
+++ src/views/Root/Root.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Button from 'components/atoms/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
+import { theme } from 'theme/mainTheme';
+
+const Root = () => (
+  <div>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <>
+        <Button>Close / Save</Button>
+        <Button secondary>Remove</Button>
+      </>
+    </ThemeProvider>
+  </div>
+);
+
+export default Root;
